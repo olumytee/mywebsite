58ace35a21b1ecf04b43563d49f02a5a2e4f232d
unknown <olumytee.ojo@gmail.com>
2016-04-04 23:44:17 +0100

    you dont need to know

--- client/client.js
+++ client/client.js
@@ -1,23 +1,40 @@
+
+Template.contact.helpers({
+  isLoading: function(){
+     if (Session.get("sendButton") == true) {
+       return true;
+     } else {
+       return false;
+     }
+  }
+});
+
+
+
+
 Template.contact.events({
         'submit form': function(event, template){
                 event.preventDefault();
+                  Session.set("sendButton", true);
                 var name = event.target.name.value;
                 var email = event.target.email.value;
                 var text = event.target.text.value;
                 Meteor.call('contactMessage', email, name, text, function(error, result){
-                
+
                 if (error){
-                        Materialize.toast('Message not sent :( Please try again', 6000) // 4000 is the duration of the toast
+                        Materialize.toast('Message not sent :( Please try again', 6000)
+                        // 4000 is the duration of the toast
+                          Session.set("sendButton", undefined);
                 } else {
-                        
+
                         Materialize.toast('Message sent! :D', 6000) // 4000 is the duration of the toast
-                
+                        Session.set("sendButton", undefined);
                 }
-                                
+
                 });
-                
-                
-        
+
+
+
         }
 
 
@@ -26,10 +43,10 @@ Template.contact.events({
 
 Handlebars.registerHelper("formatDate", function(datetime){
     if (moment) {
-   
+
          return moment(datetime).format("Do MMMM, YYYY");
-        
-    
+
+
     }
     else {
     return datetime;
